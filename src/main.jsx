f2be06b1cfd86f307cdb27037a5ed66f3f2394e5
Mohammad Al Hassan <mhmdalhassan66@gmail.com>
2026-05-02 18:48:21 +0300

    fix vercel routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
@@ -8,12 +8,12 @@ import { PortfolioProvider } from "./context/PortfolioContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/MohammadHs-Portfolio/">
       <ThemeProvider>
         <PortfolioProvider>
           <App />
         </PortfolioProvider>
       </ThemeProvider>
-    </HashRouter>
-  </StrictMode>,
+    </BrowserRouter>
+  </StrictMode>
 );
